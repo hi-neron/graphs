57c04d694af9f55ab09becf1d4c197ae97841f15
Jose Sanchez <mutatipo@MacBook-Pro-de-Jose.local>
2018-06-07 15:22:18 -0500

    follow mouse

--- src/app.js
+++ src/app.js
@@ -2,3 +2,21 @@
 import './_scss/main.scss'
 import html from 'choo/html'
 import content from './js'
+
+let canvas = document.createElement('canvas')
+canvas.setAttribute('id', 'draw')
+canvas.setAttribute('width', 1000)
+canvas.setAttribute('height', 1000)
+
+document.body.appendChild(canvas)
+
+let ctx = canvas.getContext("2d")
+ctx.font = "16px Arial"
+
+canvas.addEventListener('mousemove', (e) => {
+  let cRect = canvas.getBoundingClientRect()
+  let canvasX = Math.round(e.clientX - cRect.left)
+  let canvasY = Math.round(e.clientY - cRect.top)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.fillText(`x:${canvasX}, y:${canvasY}`, canvasX, canvasY)
+})
